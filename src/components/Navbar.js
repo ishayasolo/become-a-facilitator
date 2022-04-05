2d9d13290fc9ab8cef8d46bd02e483cf18510de3
ishayasolo <ishaya808@gmail.com>
2022-04-05 17:00:39 +0100

    embed <nav> tag in <header> tag

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,15 +3,17 @@ import logo from "../images/the-bulb-logo.png";
 
 export const Navbar = () => {
   return (
-    <nav className="nav">
-      <img className="logo" src={logo} alt="" />
-      <ul>
-        <li><a href="#">About</a></li>
-        <li><a href="#">Courses</a></li>
-        <li><a href="#">Become a facilitator</a></li>
-        <li><a href="#">FAQ</a></li>
-        <li><button>Apply now</button></li>
-      </ul>
-    </nav>
+    <header>
+      <nav className="nav">
+        <img className="logo" src={logo} alt="" />
+        <ul>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Courses</a></li>
+          <li><a href="#">Become a facilitator</a></li>
+          <li><a href="#">FAQ</a></li>
+          <li><button>Apply now</button></li>
+        </ul>
+      </nav>
+    </header>
   );
 }
