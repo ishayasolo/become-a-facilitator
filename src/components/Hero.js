0254e229e2db23c8ab9ac166b352f47b651da543
ishayasolo <ishaya808@gmail.com>
2022-04-06 10:57:02 +0100

    complete cat-phrase section

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,16 +3,22 @@ import heroImage from "../images/hero-img.webp";
 
 export const Hero = () => {
   return (
-    <main className="hero">
-      <div className="text-box">
-        <small>JOIN OUR FACULTY</small>
-        <h1>Make a global impact by training the next wave of tech experts.</h1>
-        <p>Help aspiring tech professionals level up and discover their talents through one-on-one training.</p>
-        <button>Apply to join our faculty</button>
-      </div>
-      <div className="image-box">
-        <img src={heroImage} alt="" />
-      </div>
+    <main>
+      <section className="hero">
+        <div className="text-box">
+          <small>JOIN OUR FACULTY</small>
+          <h1>Make a global impact by training the next wave of tech experts.</h1>
+          <p>Help aspiring tech professionals level up and discover their talents through one-on-one training.</p>
+          <button className="cta">Apply to join our faculty</button>
+        </div>
+        <div className="image-box">
+          <img src={heroImage} alt="" />
+        </div>
+      </section>
+      <section className="catch-phrase">
+        <h2 className="question">Ready to unlock potentials as a member of our faculty?</h2>
+        <p className="advice">Take advantage of our fellowship program to create the next batch of competent tech talents.</p>
+      </section>
     </main>
   );
 }
