3ade00e38f867c06882f7a3e235f9e5060091244
ishayasolo <ishaya808@gmail.com>
2022-04-11 14:02:10 +0100

    fix minor bugs

--- src/pages/components/Navbar.js
+++ src/pages/components/Navbar.js
@@ -3,15 +3,8 @@ import logo from "../../images/the-bulb-logo.png";
 import darkLogo from "../../images/the-bulb-logo-dark.png";
 
 import { Link } from "react-router-dom";
-import { useState } from "react";
 
 export const Navbar = () => {
-  const [open, setOpen] = useState(false);
-
-  const toggleNav = () => {
-    setOpen(!open);
-  }
-  
   return (
     <nav className="nav">
       <div className="img-container">
@@ -30,15 +23,17 @@ export const Navbar = () => {
 
 export const FormNavbar = () => {
   return (
-    <nav className="form-nav">
-      <div className="form-img-container">
-        <Link to="/become-a-facilitator"><img className="logo" src={darkLogo} alt="" /></Link>
+    <nav className="nav-2">
+      <div className="form-nav">
+        <div className="form-img-container">
+          <Link to="/become-a-facilitator"><img className="logo" src={darkLogo} alt="" /></Link>
+        </div>
+        <ul>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/courses">Courses</Link></li>
+          <li><Link to="/faq">FAQ</Link></li>
+        </ul>
       </div>
-      <ul>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/courses">Courses</Link></li>
-        <li><Link to="/faq">FAQ</Link></li>
-      </ul>
     </nav>
   );
 }
